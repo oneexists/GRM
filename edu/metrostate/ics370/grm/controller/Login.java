5baca15dac3b19dc32aced8401511542d63da5f8
Skylar Lynner <skylar.lynner@gmail.com>
2021-10-10 21:31:17 -0500

    initial abstract Login class

---
+++ edu/metrostate/ics370/grm/controller/Login.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package edu.metrostate.ics370.grm.controller;
+
+import java.sql.Connection;
+
+/**
+ * @author skylar
+ *
+ */
+public abstract class Login {	
+	public static Connection con;
+	
+}
