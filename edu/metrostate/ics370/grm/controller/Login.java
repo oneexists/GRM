5d8e49f684f4bb328534fa99db55415fd97e05ac
Skylar Lynner <skylar.lynner@gmail.com>
2021-10-11 00:10:45 -0500

    revise Login class to start to include methods

--- edu/metrostate/ics370/grm/controller/Login.java
+++ edu/metrostate/ics370/grm/controller/Login.java
@@ -1,15 +1,37 @@
-/**
- * 
- */
 package edu.metrostate.ics370.grm.controller;
-
-import java.sql.Connection;
 
 /**
  * @author skylar
  *
  */
-public abstract class Login {	
-	public static Connection con;
+public class Login {
+
+	/**
+	 * No-arg constructor
+	 */
+	public Login() {
+
+	}	
 	
+	/**
+	 * Opens login GUI, gets credentials, signs in
+	 * If sign in successful, open menu
+	 * Otherwise display error and allow user to sign in again
+	 */
+	public void openLogin() {
+		String user = null;
+		String password = null;
+		
+		// TODO get credentials
+		
+		// sign in
+		while ( !(Connector.signIn(user, password)) ) {
+			// TODO display error message
+		}
+		openMenu();
+	}
+
+	public void openMenu() {
+		// TODO
+	}
 }
