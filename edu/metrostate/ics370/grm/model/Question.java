4e6899aea461a641d0f5e77f50eb7309ffb6b159
Skylar Lynner <skylar.lynner@gmail.com>
2021-11-13 21:09:16 -0600

    modify Question class for QuestionnaireInterface implementation

--- edu/metrostate/ics370/grm/model/Question.java
+++ edu/metrostate/ics370/grm/model/Question.java
@@ -3,6 +3,8 @@
  */
 package edu.metrostate.ics370.grm.model;
 
+import java.util.ArrayList;
+
 /**
  * @author skylar
  * @author christian
@@ -12,6 +14,17 @@ public class Question {
 	private String prompt;
 	private QuestionChoice[] choices;
 
+	/**
+	 * No-arg constructor
+	 */
+	public Question() {
+	}
+	/**
+	 * @param prompt the prompt to set
+	 */
+	public void setPrompt(String prompt) {
+		this.prompt = prompt;
+	}
 	/**
 	 * Initializes the Question
 	 * 
@@ -35,5 +48,16 @@ public class Question {
 	 */
 	public QuestionChoice[] getChoices() {
 		return choices;
+	}
+	public void addChoice(QuestionChoice newChoice) {
+		// TODO Auto-generated method stub
+		ArrayList<QuestionChoice> newChoices = new ArrayList<QuestionChoice>();
+		if (choices != null) {
+			for (QuestionChoice choice : choices) {
+				newChoices.add(choice);
+			}			
+		}
+		newChoices.add(newChoice);
+		choices = newChoices.toArray(new QuestionChoice[newChoices.size()]);
 	}
 }
