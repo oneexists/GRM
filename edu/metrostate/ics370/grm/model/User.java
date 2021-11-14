ced2ca2aaa92d222f76690413516365d17299ad1
Skylar Lynner <skylar.lynner@gmail.com>
2021-11-14 00:02:43 -0600

    update User class to use ArrayList instead of TreeSet

--- edu/metrostate/ics370/grm/model/User.java
+++ edu/metrostate/ics370/grm/model/User.java
@@ -1,24 +1,16 @@
 package edu.metrostate.ics370.grm.model;
 
-import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.TreeSet;
 
 /**
  * @author skylar
  */
-public class User implements Serializable, Comparable<User> {
+public class User {
 	public enum Gender {
 		Male, Female, Other
 	}
-
-	/**
-	 * Version of the bean
-	 */
-	private static final long serialVersionUID = 202111002L;
-	private static final Comparator<User> USER_COMPARATOR = Comparator.comparing(User::getUsername);
 	
 	private String firstName;
 	private String lastName;
@@ -33,9 +25,7 @@ public class User implements Serializable, Comparable<User> {
 	 * No-arg constructor
 	 */
 	public User() {
-		personalTags = new TreeSet<GameTag>();
-		wishlist = new TreeSet<Game>();
-		hatelist = new TreeSet<Game>();
+
 	}
 
 	/**
@@ -53,6 +43,9 @@ public class User implements Serializable, Comparable<User> {
 		this.lastName = lastName;
 		this.dateOfBirth = dateOfBirth;
 		this.gender = gender;
+		personalTags = new ArrayList<GameTag>();
+		wishlist = new ArrayList<Game>();
+		hatelist = new ArrayList<Game>();
 	}
 
 	/**
@@ -63,17 +56,14 @@ public class User implements Serializable, Comparable<User> {
 	/**
 	 * @param tag to add to user
 	 */
-	public void addPersonalTags(GameTag tag) { this.personalTags.add(tag); }
+	public void addPersonalTags(GameTag tag) { 
+		this.personalTags.add(tag); 
+	}
 
 	/**
 	 * @param game to add to the wishlist of the user
 	 */
 	public void addWishlist(Game game) { this.wishlist.add(game); }
-
-	@Override
-	public int compareTo(User user) {
-		return USER_COMPARATOR.compare(this, user);
-	}
 
 	/**
 	 * @return the dateOfBirth of the user
@@ -108,7 +98,12 @@ public class User implements Serializable, Comparable<User> {
 	/**
 	 * @return the personalTags of the user
 	 */
-	public GameTag[] getPersonalTags() { return personalTags.toArray(new GameTag[personalTags.size()]); }
+	public GameTag[] getPersonalTags() { 
+		if (personalTags != null) {
+			return personalTags.toArray(new GameTag[personalTags.size()]); 			
+		}
+		return null;
+	}
 
 	/**
 	 * @return the username of the user
